aac8ae9778eb414fbead6ebf82299921fba771c3
Mary Wachira <waitheramary7532@gmail.com>
2023-08-14 02:26:55 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,12 @@ int main(void)
 	c = 'a';
 	while
 		(c <= 'z') {
-			if ((c != 'q' && c != 'e') && c <= 'z')
-				putchar(c);
+			if (c != 'q' && c != 'e')
+			{
+			putchar(c);
+			}
 			c++;
 		}
 	putchar('\n');
 	return (0);
+}
