6992134f8d5d6b2e53056b94e154c06cd5ac5072
Mary Wachira <waitheramary7532@gmail.com>
2023-08-16 00:26:25 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,16 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
+		c = 'a';
+
+		while (c <= 'z')
 		{
 			_putchar(c);
-
-			_putchar('\n');
-			i++;
+			c++;
 		}
+		_putchar('\n');
+		i++;
 	}
 }
